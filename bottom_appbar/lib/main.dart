1b1e2420dc32233d4d489f6fdbb9c555741ebbb4
SheikhAman <amansk664@gmail.com>
2022-04-12 21:18:13 +0600

    bottom_appbar

--- bottom_appbar/lib/main.dart
+++ bottom_appbar/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,103 +14,129 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: BottomAppbar(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class BottomAppbar extends StatefulWidget {
+  const BottomAppbar({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _BottomAppbarState createState() => _BottomAppbarState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _BottomAppbarState extends State<BottomAppbar> {
+  FloatingActionButtonLocation _fabLocation =
+      FloatingActionButtonLocation.endDocked;
+  bool _isBottomBarNotched = false;
+  bool _isFabMini = false;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: ListView(
+        padding: EdgeInsets.all(10),
+        children: <Widget>[
+          ListTile(
+            title: Text('FloatingActionButton position:'),
+            trailing: DropdownButton<FloatingActionButtonLocation>(
+              value: _fabLocation,
+              onChanged: (FloatingActionButtonLocation? newVal) {
+                if (newVal != null) {
+                  setState(() => this._fabLocation = newVal);
+                }
+              },
+              items: [
+                DropdownMenuItem(
+                  value: FloatingActionButtonLocation.centerFloat,
+                  child: Text('centerFloat'),
+                ),
+                DropdownMenuItem(
+                  value: FloatingActionButtonLocation.endFloat,
+                  child: Text('endFloat'),
+                ),
+                DropdownMenuItem(
+                  value: FloatingActionButtonLocation.centerDocked,
+                  child: Text('centerDocked'),
+                ),
+                DropdownMenuItem(
+                  value: FloatingActionButtonLocation.endDocked,
+                  child: Text('endDocked'),
+                ),
+                DropdownMenuItem(
+                  value: FloatingActionButtonLocation.miniStartTop,
+                  child: Text('miniStartTop'),
+                ),
+                DropdownMenuItem(
+                  value: FloatingActionButtonLocation.startTop,
+                  child: Text('startTop'),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          ListTile(
+            title: Text('Mini FAB:'),
+            trailing: Switch(
+              value: this._isFabMini,
+              onChanged: (bool val) {
+                setState(() => this._isFabMini = val);
+              },
             ),
-          ],
-        ),
+          ),
+          ListTile(
+            title: Text('BottomAppBar notch:'),
+            trailing: Switch(
+              value: this._isBottomBarNotched,
+              onChanged: (bool val) {
+                setState(() => this._isBottomBarNotched = val);
+              },
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: Icon(Icons.add),
+        mini: this._isFabMini,
+        onPressed: () =>
+            Fluttertoast.showToast(msg: 'Dummy floating action button'),
+      ),
+      floatingActionButtonLocation: this._fabLocation,
+      bottomNavigationBar: this._buildBottomAppBar(context),
+    );
+  }
+
+  BottomAppBar _buildBottomAppBar(BuildContext context) {
+    return BottomAppBar(
+      shape: this._isBottomBarNotched ? CircularNotchedRectangle() : null,
+      color: Theme.of(context).primaryColor,
+      child: Row(
+        children: <Widget>[
+          // Bottom that pops up from the bottom of the screen.
+          IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () => showModalBottomSheet(
+              context: context,
+              builder: (BuildContext context) => Container(
+                alignment: Alignment.center,
+                height: 200,
+                child: Text('Dummy bottom sheet'),
+              ),
+            ),
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () =>
+                Fluttertoast.showToast(msg: 'Dummy search action.'),
+          ),
+          IconButton(
+            icon: const Icon(Icons.more_vert),
+            onPressed: () => Fluttertoast.showToast(msg: 'Dummy menu action.'),
+          ),
+        ],
+      ),
     );
   }
 }
